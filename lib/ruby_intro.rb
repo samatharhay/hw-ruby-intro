ebe84d14e90f4c3d8ba8e2e031a7d0778f51acb3
samatharhay <55367197+samatharhay@users.noreply.github.com>
2019-09-16 15:45:03 -0500

    finished hw

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,65 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+	sum = 0
+	arr.each do |i|
+		sum+=i
+	end
+	return sum
+
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  sum(arr.max(2))
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+	i=0
+	while i < arr.length
+		j = i+1
+		while j < arr.length
+			if(arr[i]+arr[j] == n)
+				return true
+			end
+		j+=1
+		end
+	i+=1
+	end 
+	return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+	if (s =~ /\A(?=[^aeiou])(?=[a-z])/i) == nil
+		return false
+	end
+	return true
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if ((s.to_i(2) % 4) == 0) && (s =~ /\b[01]+\b/)
+	return true
+  end
+  return false
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+	attr_accessor :isbn, :price
+	
+	def initialize(_isbn,_price)
+	raise ArgumentError if (_isbn.empty? || _price <=0)
+	@isbn = _isbn
+	@price = Float(_price)
+	end
+	
+	def price_as_string
+	"$" + sprintf('%.2f',price)
+	end
 end
